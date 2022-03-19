3ec09fa3e68cfedd5706418c5c580b2211048a23
tibor.digana <tibordigana@apache.org>
2022-03-19 21:16:31 +0100

    project

---
+++ src/test/java/pkg/BDSHelperTest.java
@@ -0,0 +1,13 @@
+package pkg;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BDSHelperTest {
+    @Test
+    public void test() {
+
+    }
+}

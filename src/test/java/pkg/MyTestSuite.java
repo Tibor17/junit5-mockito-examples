c87038b8154ae908ff50bd84e19776dfbddbe779
tibor.digana <tibordigana@apache.org>
2022-03-20 14:27:13 +0100

    added a profile with JUnit5 Suites

---
+++ src/test/java/pkg/MyTestSuite.java
@@ -0,0 +1,9 @@
+package pkg;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses(BDSHelperTest.class)
+public class MyTestSuite {
+}
